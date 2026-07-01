21eaef5838415b95046db32eae9c9caebb584785
dengkaitraining <dengkai.training@gmail.com>
2026-07-01 11:54:12 +0800

    2026-07-01-（08:00 ~ 12:00）Python Flask 框架（徐偉智）Class 4

---
+++ 115-07-01/course/ex003.py
@@ -0,0 +1,14 @@
+"""
+scope
+"""
+x = 300
+
+def myfunc():
+    y = 1000
+    print("inside x =", x)
+    print("inside y =", y)
+
+myfunc()
+
+print("x = ", x)
+print("y = ", y) # 因為 y 定義於 myfunc 內，所以照不到該數值。
---
+++ 115-07-01/course/ex005-2d-arary.py
@@ -0,0 +1,9 @@
+"""
+numpy 並未視線建立於 python module 列表，所以必須線安裝該模組。
+指令如下：
+pip install numpy
+"""
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("arr = ", arr)
